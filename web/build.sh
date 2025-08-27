aafb396c636791e33f560eee97b4d0315967df30
王云海 <adoin@qq.com>
2025-08-27 17:21:39 +0800

    按es5编译--target=es5

--- web/build.sh
+++ web/build.sh
@@ -13,7 +13,7 @@ for file in js/*.mjs js/worker/*.mjs; do
   output_file=$(echo "${file}" | sed 's/\.mjs$/.js/')
   
   # Bundle all dependencies (including @aws-crypto/sha256-js for purejs worker)
-  esbuild "${file}" --sourcemap --target=es2015 --format=cjs --bundle --minify --outfile=static/"${output_file}"
+  esbuild "${file}" --sourcemap --target=es5 --format=cjs --bundle --minify --outfile=static/"${output_file}"
   
   gzip -f -k -n static/${output_file}
   zstd -f -k --ultra -22 static/${output_file}
