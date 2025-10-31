4c2d9ee2dbe1757315e0b9f59099aba9aa61f6b5
Adoin Wang <adoin.wang@igg.com>
2025-10-30 21:32:05 -0500

    服务器编译

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
 make all
+CURRENT_DATE=$(date +%Y%m%d)
 docker build -t anubis-ng:main .
-docker tag anubis-ng:main cargo.skyunion.net/public/anubis:20251014
-docker push cargo.skyunion.net/public/anubis:20251014
+docker tag anubis-ng:main cargo.skyunion.net/public/anubis:$CURRENT_DATE
+docker push cargo.skyunion.net/public/anubis:$CURRENT_DATE
